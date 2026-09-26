99bd4e0173e822782c2119f97df43d3512ab7eb0
Hyria Roman <hyrya.roman@gmail.com>
2026-09-26 21:15:43 +0300

    ui-kit/icons: add all icons

--- app/(public routes)/markup/page.tsx
+++ app/(public routes)/markup/page.tsx
@@ -4,12 +4,82 @@ import css from "./page.module.css";
 import Button from "@/components/ui/Button/Button";
 import Input from "@/components/ui/Input/Input";
 import Textarea from "@/components/ui/Textarea/Textarea";
+import Icon from "@/components/ui/Icon/Icon";
 
 export default function Home() {
   return (
     <main className={css.main}>
       <div className={clsx("container")}>
         <h1 className={clsx("main-headding")}>Markup</h1>
+        <ul style={{display: "flex"}}>
+          <li>
+            <Icon name="youtube" />
+          </li>
+          <li>
+            <Icon name="twitter" />
+          </li>
+          <li>
+            <Icon name="star_empty" />
+          </li>
+          <li>
+            <Icon name="star_half" />
+          </li>
+          <li>
+            <Icon name="star_filled" />
+          </li>
+          <li>
+            <Icon name="select_check_box" />
+          </li>
+          <li>
+            <Icon name="menu" />
+          </li>
+          <li>
+            <Icon name="map_search" />
+          </li>
+          <li>
+            <Icon name="logout" />
+          </li>
+          <li>
+            <Icon name="keyboard_arrow_up" />
+          </li>
+          <li>
+            <Icon name="keyboard_arrow_down" />
+          </li>
+          <li>
+            <Icon name="instagram" />
+          </li>
+          <li>
+            <Icon name="filter_alt" />
+          </li>
+          <li>
+            <Icon name="facebook" />
+          </li>
+          <li>
+            <Icon name="edit" />
+          </li>
+          <li>
+            <Icon name="communication" />
+          </li>
+          <li>
+            <Icon name="close" />
+          </li>
+          <li>
+            <Icon name="chevron_right" />
+          </li>
+          <li>
+            <Icon name="chevron_left" />
+          </li>
+          <li>
+            <Icon name="bookmark" />
+          </li>
+          <li>
+            <Icon name="arrow_forward" />
+          </li>
+          <li>
+            <Icon name="arrow_back" />
+          </li>
+        </ul>
+        <p></p>
         <ul>
           <li>
             <Button variant="primary">Primary Button</Button>
--- components/ui/Icon/Icon.tsx
+++ components/ui/Icon/Icon.tsx
@@ -1,14 +1,9 @@
 interface IconProps extends React.SVGProps<SVGSVGElement> {
-  name: string;
+  name: "youtube" | "twitter" | "star_empty" | "star_half" | "star_filled" | "select_check_box" | "menu" | "map_search" | "logout" | "keyboard_arrow_up" | "keyboard_arrow_down" | "instagram" | "filter_alt" | "facebook" | "edit" | "communication" | "close" | "chevron_right" | "chevron_left" | "bookmark" | "arrow_forward" | "arrow_back";
   size?: number;
 }
 
-export default function Icon({
-  name,
-  size = 24,
-  className,
-  ...props
-}: IconProps) {
+export default function Icon({ name, size = 24, className, ...props }: IconProps) {
   return (
     <svg width={size} height={size} className={className} {...props}>
       <use href={`/icons/sprite.svg#${name}`}></use>
