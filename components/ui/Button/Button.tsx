7198cea155e9bc4e605ce0ff92e9067ad76f13e5
Hyria Roman <hyrya.roman@gmail.com>
2026-09-26 15:53:59 +0300

    ui-kit/button: make props extend html button attributes

--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -2,23 +2,17 @@ import clsx from "clsx";
 import "../common.module.css";
 import css from "./Button.module.css";
 
-interface ButtonProps {
-  type?: "submit" | "reset" | "button";
-  onClick?: () => void;
-  children: React.ReactNode;
-
-  disabled?: boolean;
-  className?: string;
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: "primary" | "secondary" | "outline" | "ghost";
   size?: "sm" | "md" | "lg";
 }
 
-export default function Button({ type, onClick, children, disabled, className, variant = "primary", size = "md" }: ButtonProps): React.ReactNode {
+export default function Button({ children, className, variant = "primary", size = "md", ...props }: ButtonProps): React.ReactNode {
   const variantClass = css[`button--variant-${variant}`];
   const sizeClass = css[`button--size-${size}`];
 
   return (
-    <button className={clsx(css.button, variantClass, sizeClass, className)} disabled={disabled} type={type} onClick={onClick}>
+    <button {...props} className={clsx(css.button, variantClass, sizeClass, className)}>
       {children}
     </button>
   );
